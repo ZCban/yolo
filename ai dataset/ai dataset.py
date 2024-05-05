4bc0105e4085ebac93c4821fec4ce954868d354f
ZCban <100644497+ZCban@users.noreply.github.com>
2024-05-05 09:45:32 +0200

    Update ai dataset.py

--- ai dataset/ai dataset.py
+++ ai dataset/ai dataset.py
@@ -8,42 +8,52 @@ from PIL import Image
 from ultralytics import YOLO
 import ssl
 
-video1_name = 'valorant'
-num_videos = 1
+video1_name = 'f1 23'
+num_videos = 5
 # Dimensione desiderata per il ridimensionamento dei frame
-resize_dimension = 640 #dimesion for  cropped image
-saveimageevrynumframe=10 #save evry 10 frames
+resize_dimension = 1024 #dimesion for  cropped image
+saveimageevrynumframe=30 #save evry 10 frames
 ssl._create_default_https_context = ssl._create_unverified_context
 
-model = YOLO('yolov8n.pt')
+model = YOLO('best.pt')
 
 
-def findvideo():
+def search_videos(video_name, num_results):
+    script_dir = os.path.dirname(__file__)
+    log_file = os.path.join(script_dir, "log.txt")
+
+    if os.path.isfile(log_file):
+        with open(log_file, "r") as f:
+            downloaded_titles = f.read().splitlines()
+    else:
+        downloaded_titles = []
+
+    video_list = pytube.Search(video_name).results
+    if not video_list:
+        print("No videos found with the specified name")
+        return []
+    
+    # Filter videos that have not been downloaded yet
+    video_list = [video for video in video_list if video.title not in downloaded_titles]
+    return video_list[:num_results]
+
+def download_videos(videos):
+    """
+    Download specified videos.
+    """
     script_dir = os.path.dirname(__file__)
     download_path = os.path.join(script_dir, "download")
     if not os.path.exists(download_path):
         os.makedirs(download_path)
     log_file = os.path.join(script_dir, "log.txt")
-    if os.path.isfile(log_file):
-        with open(log_file, "r") as f:
-            downloaded_files = f.read().splitlines()
-    else:
-        downloaded_files = []
 
-    video_list = pytube.Search(video1_name).results
-    while not video_list:
-        print("Nessun video trovato con il nome specificato")
-        return
-    video_list = [v for v in video_list if v.title not in downloaded_files]
-    video_list = video_list[:num_videos]
-
-    for video1 in tqdm(video_list, desc="Download progress", unit="video"):
-        yt = pytube.YouTube(video1.watch_url)
-        #stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').desc().first()
+    for video in tqdm(videos, desc="Download progress", unit="video"):
+        yt = pytube.YouTube(video.watch_url)
         stream = yt.streams.get_highest_resolution()
         stream.download(download_path)
         with open(log_file, "a", encoding="utf-8") as f:
-            f.write(video1.title + "\n")
+            f.write(video.title + "\n")
+
 
 def video_to_frame(target_size, frames_to_skip=100):
     videos_folder = "download"
@@ -89,14 +99,14 @@ def video_to_frame(target_size, frames_to_skip=100):
     cv2.destroyAllWindows()
 
 
-def mantieni0_25():
+def mantieni0_35():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     input_folder = os.path.join(script_dir, "frames_extracted")
 
     for filename in tqdm(os.listdir(input_folder)):
         image_path = os.path.join(input_folder, filename)
         image = Image.open(image_path)#.convert("RGB")
-        results = model.predict(image, classes=[0,], conf=0.25)
+        results = model.predict(image, classes=[0,], conf=0.20)
         targets = []
         # Loop attraverso i risultati della predizione
         for result in results:
@@ -116,7 +126,7 @@ def elimino0_50():
     for filename in tqdm(os.listdir(input_folder)):
         image_path = os.path.join(input_folder, filename)
         image = Image.open(image_path)#.convert("RGB")
-        results = model.predict(image, classes=[0,], conf=0.48)
+        results = model.predict(image, classes=[0,], conf=0.51)
         targets = []
         # Loop attraverso i risultati della predizione
         for result in results:
@@ -136,7 +146,7 @@ def mantieni0_25SEG():
     for filename in tqdm(os.listdir(input_folder)):
         image_path = os.path.join(input_folder, filename)
         image = Image.open(image_path)#.convert("RGB")
-        results = model.predict(image, classes=[0,], conf=0.25)
+        results = model.predict(image, classes=[1,], conf=0.25)
         targets = []
         # Loop attraverso i risultati della predizione
         for result in results:
@@ -157,17 +167,17 @@ def elimino0_50SEG():
         for result in results:
             if result.masks is not None:
                 os.remove(image_path)
-                
-while True:
-    print('cercando video')
-    findvideo()
-    print('catturo frame')
-    video_to_frame(target_size=resize_dimension, frames_to_skip=saveimageevrynumframe)
-    print('0.35')
-    mantieni0_35()
-    print('0-50')
-    elimino0_50()
 
+
+
+video_list = search_videos(video1_name, num_videos)
+if video_list:
+    download_videos(video_list)
+video_to_frame(target_size=resize_dimension, frames_to_skip=saveimageevrynumframe)
+#elimino0_50()
+#mantieni0_35()
+mantieni0_25SEG()
+elimino0_50SEG()
 
 
 
