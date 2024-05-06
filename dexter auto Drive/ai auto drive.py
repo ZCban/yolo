068b47195825e97e9d646f8197974b34049105f0
ZCban <100644497+ZCban@users.noreply.github.com>
2024-05-06 22:46:40 +0200

    Update ai auto drive.py

--- dexter auto Drive/ai auto drive.py
+++ dexter auto Drive/ai auto drive.py
@@ -10,14 +10,18 @@ import time
 kmNet.init('192.168.2.188','1408','9FC05414')
 # Load the YOLO model
 model = YOLO("best.pt")
+visual=True
 
-# Camera initialization
-cam = bettercam.create(output_idx=0, output_color="BGR")
-cam.start(video_mode=True, target_fps=60)
-conf = 0.45
-desired_size = (352, 352)  # Desired size for resizing
-center_x_screenshot = desired_size[0] // 2
-center_y_screenshot = desired_size[1] // 2 + 50
+screenshot = 540
+left, top = (1920 - screenshot) // 2, (1080 - screenshot) // 2
+right, bottom = left + screenshot, top + screenshot
+region = (left, top, right, bottom)
+cam=bettercam.create(output_idx=0, output_color="BGR")
+cam.start(region=region,video_mode=True,target_fps=60)#region=region
+
+conf = 0.3
+
+center_x_screenshot=screenshot//2
 
 # Define colors for each class in BGR format
 colors = {
@@ -29,7 +33,6 @@ colors = {
 
 while True:
     img = cam.get_latest_frame()
-    img = cv2.resize(img, desired_size)
 
     results = model.predict(img, conf=conf, classes=[1])
 
@@ -37,52 +40,73 @@ while True:
         if result.masks is not None:
             for mask in result.masks.xy:
                 points = np.int32([mask])
-                cv2.fillPoly(img, [points], (0, 255, 0))
 
+                
                 # Calculate the centroid of the polygon
                 M = cv2.moments(points)
-                if M["m00"] != 0:
-                    center_x = int(M["m10"] / M["m00"])
-                    center_y = int(M["m01"] / M["m00"])
-                else:
-                    center_x, center_y = 0, 0
-
-                # Draw the centroid
-                cv2.circle(img, (center_x, center_y), 5, (0, 0, 255), -1)
+                #if M["m00"] != 0:
+                center_x = int(M["m10"] / M["m00"])
+                center_y = int(M["m01"] / M["m00"]) 
+                #else:
+                    #center_x, center_y = 0, 0                               
 
                 # Calculate the error in the x-axis
                 error_x = center_x - center_x_screenshot
-                if win32api.GetKeyState(0x14):
+
+                if win32api.GetKeyState(0x05)<0:
                     # Adjust the car direction based on error_x
-                    if error_x > 1:  # Move to the right
+                    if error_x > 14:  # Move to the right
+                        kmNet.keydown(0x16)
                         kmNet.keyup(0x1A)
-                        kmNet.keydown(0x07)
-                        time.sleep(0.05)
-                        kmNet.keyup(0x07)
-                    elif error_x < -1:  # Move to the left
-                        kmNet.keyup(0x1A)
-                        kmNet.keydown(0x04)
-                        time.sleep(0.05)
                         kmNet.keyup(0x04)
+                        kmNet.keyup(0x16)
+                        kmNet.keydown(0x07)
+                        
+
+                    elif error_x < -14:  # Move to the left
+                        kmNet.keydown(0x16)
+                        kmNet.keyup(0x1A)
+                        kmNet.keyup(0x07)                        
+                        kmNet.keyup(0x16)
+                        kmNet.keydown(0x04)
+                        
+
 
                     else:  # Center aligned, go straight
+                        kmNet.keyup(0x16)
+                        kmNet.keyup(0x07)
+                        kmNet.keyup(0x04)
                         kmNet.keydown(0x1A)
+                else:
+                    kmNet.keyup(0x16)
+                    kmNet.keyup(0x07)
+                    kmNet.keyup(0x04)
+                    kmNet.keyup(0x1A)
+
+                    
+
+                if visual:
+                    #draw target detection model
+                    cv2.fillPoly(img, [points], (0, 255, 0))
+                    # Draw the centroid
+                    cv2.circle(img, (center_x, center_y), 5, (0, 0, 255), -1)
 
 
 
-    # Draw a red vertical line at the center of the screenshot
-    #cv2.line(img, (center_x_screenshot, 0), (center_x_screenshot, img.shape[0]), (0, 0, 255), thickness=2)
-    # Draw a red horizontal line at the center of the screenshot/2 + 50
-    #cv2.line(img, (0, center_y_screenshot), (img.shape[1], center_y_screenshot), (0, 0, 255), thickness=2)
-
-    # Display the image in a window
-    cv2.imshow("show", img)
-    cv2.setWindowProperty("show", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.waitKey(1)
+    if visual:
+        #show image 
+        cv2.imshow("show", img)
+        cv2.waitKey(1)
 
 
 
 cv2.destroyAllWindows()
+
+
+
+
+
+
 
 
 
