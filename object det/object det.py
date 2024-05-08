65a06303c0e56583c4516120b15a0a34e328a2f5
ZCban <100644497+ZCban@users.noreply.github.com>
2024-05-08 10:13:57 +0200

    Update object det.py

--- object det/object det.py
+++ object det/object det.py
@@ -14,16 +14,8 @@ center=screenshot/2
 # Initialize YOLOv9c model with pretrained weights
 model = YOLO('yolov9c.pt')
 
-
-def predict(chosen_model, img, classes=[], conf=0.5):
-    if classes:
-        results = chosen_model.predict(img, classes=classes, conf=conf)
-    else:
-        results = chosen_model.predict(img, conf=conf)
-    return results
-
 def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
-    results = predict(chosen_model, img, classes, conf=conf)
+    results = model.predict(img, classes, conf=conf)
     for result in results:
         for box in result.boxes:
             cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
@@ -39,7 +31,7 @@ while True:
     img = cam.get_latest_frame()
 
     # Dysplay Image
-    result_img,_= predict_and_detect(model, img, classes=[0,], conf=0.5)
+    result_img,_= predict_and_detect(img, classes=[0,], conf=0.5)
     cv2.imshow('YOLO Detection', result_img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
